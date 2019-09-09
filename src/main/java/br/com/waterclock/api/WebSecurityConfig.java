fbb3843bfefd9c0df8ae063b47b42aba73c0afc7
Marcus Medeiros <vinicius1504_medeiros@hotmail.com>
2019-09-09 10:38:07 -0300

    web security with all free security

--- src/main/java/br/com/waterclock/api/WebSecurityConfig.java
+++ src/main/java/br/com/waterclock/api/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/h2-console").permitAll()
+                .antMatchers("/", "/home", "/h2/**","/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
