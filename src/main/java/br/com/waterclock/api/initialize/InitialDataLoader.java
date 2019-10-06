51eed9610205a744383e2186830e6da70e3d51cb
Kamyla Aragão <kamyla.aragao55@gmail.com>
2019-10-06 14:28:27 -0300

    load admin credentials via aplication.properties

--- src/main/java/br/com/waterclock/api/initialize/InitialDataLoader.java
+++ src/main/java/br/com/waterclock/api/initialize/InitialDataLoader.java
@@ -7,6 +7,7 @@ import br.com.waterclock.api.repository.PrivilegeRepository;
 import br.com.waterclock.api.repository.RoleRepository;
 import br.com.waterclock.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,6 +34,12 @@ public class InitialDataLoader implements
     @Autowired
     private PrivilegeRepository privilegeRepository;
 
+    @Value("${waterclock.admin.username}")
+    private String adminUsername;
+
+    @Value("${waterclock.admin.password}")
+    private String adminPassword;
+
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
@@ -50,15 +57,14 @@ public class InitialDataLoader implements
 
     @Transactional
     void createAdminUserIfNotFound() {
-        String adminEmail = "test@test.com";
-        User user = userRepository.findByEmail(adminEmail);
+        User user = userRepository.findByEmail(adminUsername);
         if (user == null) {
             Role adminRole = roleRepository.findByName("ROLE_ADMIN");
             user = new User();
-            user.setFirstName("Test");
-            user.setLastName("Test");
-            user.setPassword(passwordEncoder.encode("test"));
-            user.setEmail(adminEmail);
+            user.setFirstName("Admin");
+            user.setLastName("Admin");
+            user.setPassword(passwordEncoder.encode(adminPassword));
+            user.setEmail(adminUsername);
             user.setRoles(Arrays.asList(adminRole));
             user.setEnabled(true);
             userRepository.save(user);
