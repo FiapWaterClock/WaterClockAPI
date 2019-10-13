4e98bbd0ae3ecaf0abc4efd5a886b4b4036bb284
Kamyla Aragão <kamyla.aragao55@gmail.com>
2019-10-13 16:43:46 -0300

    consumption time

--- src/main/java/br/com/waterclock/api/model/ConsumptionModel.java
+++ src/main/java/br/com/waterclock/api/model/ConsumptionModel.java
@@ -1,8 +1,11 @@
 package br.com.waterclock.api.model;
 
+import java.time.LocalDate;
+
 public class ConsumptionModel {
     private int clockId;
     private double litersPerMinute;
+    private LocalDate time;
 
     public int getClockId() {
         return clockId;
@@ -18,5 +21,14 @@ public class ConsumptionModel {
 
     public void setLitersPerMinute(double litersPerMinute) {
         this.litersPerMinute = litersPerMinute;
+    }
+
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDate time) {
+        this.time = time;
     }
 }
